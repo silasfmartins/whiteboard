627b4956b6da36f664ded386a4d16e8e6d4a9461
Silas Martins <silas.martins2041@gmail.com>
2023-01-28 19:05:36 -0300

    First Commit

---
+++ src/components/edges/DefaultEdge.tsx
@@ -0,0 +1,33 @@
+import { EdgeProps, getSmoothStepPath } from 'reactflow';
+
+export default function DefaultEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  data,
+  markerEnd,
+}: EdgeProps) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  return (
+    <path
+      id={id}
+      style={style}
+      className="react-flow__edge-path stroke-2 stroke-zinc-300"
+      d={edgePath}
+      markerEnd={markerEnd}
+    />
+  );
+}
